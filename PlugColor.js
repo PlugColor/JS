28baf231a03b7cf712253385d3c7abf41578bcdd
ureadmyname <ureadmyemailname@gmail.com>
2015-04-05 06:09:33 +0930

    Update PlugColor.js

--- PlugColor.js
+++ PlugColor.js
@@ -2,42 +2,41 @@ $(background);
 function background(){
 $('.room-background').css('background','url("http://i.imgur.com/EFXFnql.png")');}
 // 35 different backgrounds: (Just Change the URL (above) with another URL(below))
-// background-image: url(http://i.imgur.com/cwHjuod.png)*/ /* Custom WINTER
-// background-image: url(http://i.imgur.com/kmmPWiS.jpg)*/ /* PLUG.DJ OLD DEFAULT
-// background-image: url(http://i.imgur.com/1aw3xcd.png)*/ /* Abandoned Ballroom
-// background-image: url(http://i.imgur.com/HG8mqaM.png)*/ /* Ancient Ruins
-// background-image: url(http://i.imgur.com/8hfUntO.png)*/ /* Architect Chamber
-// background-image: url(http://i.imgur.com/XjiQctM.png)*/ /* Bomb Shelter
-// background-image: url(http://i.imgur.com/jNRQXKZ.png)*/ /* Castleland (by Dionyus5)
-// background-image: url(http://i.imgur.com/ILrUcVK.png)*/ /* Chillout Mixer Christmas
-// background-image: url(http://i.imgur.com/nb4ibg4.png)*/ /* Chillout Mixer Chriatmas Lite
-// background-image: url(http://i.imgur.com/nptZvUk.png)*/ /* Chillout Mixer Theme
-// background-image: url(http://i.imgur.com/mL0fuwb.png)*/ /* Chillout Mixer Theme II
-// background-image: url(http://i.imgur.com/2Q89Rn2.png)*/ /* Christmas Classic
-// background-image: url(http://i.imgur.com/tOEACrk.png)*/ /* Digital Desert
-// background-image: url(http://i.imgur.com/zSMRtE6.png)*/ /* Digital Dungeon (Lite)
-// background-image: url(http://i.imgur.com/WTylHRy.png)*/ /* Digital Dungeon
-// background-image: url(http://i.imgur.com/6N7svVu.png)*/ /* End of Line Club
-// background-image: url(http://i.imgur.com/XZNVZmj.png)*/ /* Fairytale Land
-// background-image: url(http://i.imgur.com/9DVTnnW.png)*/ /* Mordor
-// background-image: url(http://i.imgur.com/8G44P7F.png)*/ /* (No Theme) Black
-// background-image: url(http://i.imgur.com/md2nlVM.png)*/ /* (No Theme) Mixer
-// background-image: url(http://i.imgur.com/zuRZwYR.png)*/ /* (No Theme) Plug
-// background-image: url(http://i.imgur.com/JZjGLPH.png)*/ /* Off The Grid V1
-// background-image: url(http://i.imgur.com/M4Z45oA.png)*/ /* Off The Grid V2
-// background-image: url(http://i.imgur.com/EFXFnql.png)*/ /* Orbital Lounge
-// background-image: url(http://i.imgur.com/P4GVhF4.png)*/ /* plug.dj Christmas Classic
-// background-image: url(http://i.imgur.com/M0CeHah.png)*/ /* plug.dj Chriatmas Ice
-// background-image: url(http://i.imgur.com/lK4GttQ.png)*/ /* Red Rocks Theme
-// background-image: url(http://i.imgur.com/N82wzhY.png)*/ /* Skygazer Theme
-// background-image: url(http://i.imgur.com/k9zVa92.png)*/ /* Skygazer Theme (Lite)
-// background-image: url(http://i.imgur.com/GZKgCpk.png)*/ /* TT.fm After Party Theme
-// background-image: url(http://i.imgur.com/u36VR4n.png)*/ /* TT.fm Red Theme
-// background-image: url(http://i.imgur.com/zlLjMmN.jpg)*/ /* BONUS: TASTYCAT
-// background-image: url(http://i.imgur.com/sfU5X2Q.jpg)*/ /* BONUS: HALLOWEEN '14
-// background-image: url(http://i.imgur.com/hjtFKu3.jpg)*/ /* BONUS: WINTER '13
-// background-image: url(http://i.imgur.com/S9qLJLn.jpg)*/ /* BONUS: WINTER '14
-// background finish
+// 1: background-image: url(http://i.imgur.com/cwHjuod.png) Custom WINTER
+// 2: background-image: url(http://i.imgur.com/kmmPWiS.jpg) PLUG.DJ OLD DEFAULT
+// 3: background-image: url(http://i.imgur.com/1aw3xcd.png) Abandoned Ballroom
+// 4: background-image: url(http://i.imgur.com/HG8mqaM.png) Ancient Ruins
+// 5: background-image: url(http://i.imgur.com/8hfUntO.png) Architect Chamber
+// 6: background-image: url(http://i.imgur.com/XjiQctM.png) Bomb Shelter
+// 7: background-image: url(http://i.imgur.com/jNRQXKZ.png) Castleland (by Dionyus5)
+// 8: background-image: url(http://i.imgur.com/ILrUcVK.png) Chillout Mixer Christmas
+// 9: background-image: url(http://i.imgur.com/nb4ibg4.png) Chillout Mixer Chriatmas Lite
+// 10: background-image: url(http://i.imgur.com/nptZvUk.png) Chillout Mixer Theme
+// 11: background-image: url(http://i.imgur.com/mL0fuwb.png) Chillout Mixer Theme II
+// 12: background-image: url(http://i.imgur.com/2Q89Rn2.png) Christmas Classic
+// 13: background-image: url(http://i.imgur.com/tOEACrk.png) Digital Desert
+// 14: background-image: url(http://i.imgur.com/zSMRtE6.png) Digital Dungeon (Lite)
+// 15: background-image: url(http://i.imgur.com/WTylHRy.png) Digital Dungeon
+// 16: background-image: url(http://i.imgur.com/6N7svVu.png) End of Line Club
+// 17: background-image: url(http://i.imgur.com/XZNVZmj.png) Fairytale Land
+// 18: background-image: url(http://i.imgur.com/9DVTnnW.png) Mordor
+// 19: background-image: url(http://i.imgur.com/8G44P7F.png) (No Theme) Black
+// 20: background-image: url(http://i.imgur.com/md2nlVM.png) (No Theme) Mixer
+// 21: background-image: url(http://i.imgur.com/zuRZwYR.png) (No Theme) Plug
+// 22: background-image: url(http://i.imgur.com/JZjGLPH.png) Off The Grid V1
+// 23: background-image: url(http://i.imgur.com/M4Z45oA.png) Off The Grid V2
+// 24: background-image: url(http://i.imgur.com/EFXFnql.png) Orbital Lounge
+// 25: background-image: url(http://i.imgur.com/P4GVhF4.png) plug.dj Christmas Classic
+// 26: background-image: url(http://i.imgur.com/M0CeHah.png) plug.dj Chriatmas Ice
+// 27: background-image: url(http://i.imgur.com/lK4GttQ.png) Red Rocks Theme
+// 28: background-image: url(http://i.imgur.com/N82wzhY.png) Skygazer Theme
+// 29: background-image: url(http://i.imgur.com/k9zVa92.png) Skygazer Theme (Lite)
+// 30: background-image: url(http://i.imgur.com/GZKgCpk.png) TT.fm After Party Theme
+// 31: background-image: url(http://i.imgur.com/u36VR4n.png) TT.fm Red Theme
+// 32: background-image: url(http://i.imgur.com/zlLjMmN.jpg) BONUS: TASTYCAT
+// 33: background-image: url(http://i.imgur.com/sfU5X2Q.jpg) BONUS: HALLOWEEN '14
+// 34: background-image: url(http://i.imgur.com/hjtFKu3.jpg) BONUS: WINTER '13
+// 35: background-image: url(http://i.imgur.com/S9qLJLn.jpg) BONUS: WINTER '14
 // animation of coffee (below) (working progress!...)
 // $(coffee_pic);
 // function coffee_pic(){
