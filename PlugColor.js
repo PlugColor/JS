7fcd4e6f70c755a36482a62ccac92c31b6e90646
ureadmyname <ureadmyemailname@gmail.com>
2015-04-05 05:33:06 +0930

    Create PlugColor.js

---
+++ PlugColor.js
@@ -0,0 +1,50 @@
+$(background);
+function background(){
+$('.room-background').css('background','url("http://i.imgur.com/EFXFnql.png")');}
+// 35 different backgrounds: (Just Change the URL (above) with another URL(below))
+// background-image: url(http://i.imgur.com/cwHjuod.png)*/ /* Custom WINTER
+// background-image: url(http://i.imgur.com/kmmPWiS.jpg)*/ /* PLUG.DJ OLD DEFAULT
+// background-image: url(http://i.imgur.com/1aw3xcd.png)*/ /* Abandoned Ballroom
+// background-image: url(http://i.imgur.com/HG8mqaM.png)*/ /* Ancient Ruins
+// background-image: url(http://i.imgur.com/8hfUntO.png)*/ /* Architect Chamber
+// background-image: url(http://i.imgur.com/XjiQctM.png)*/ /* Bomb Shelter
+// background-image: url(http://i.imgur.com/jNRQXKZ.png)*/ /* Castleland (by Dionyus5)
+// background-image: url(http://i.imgur.com/ILrUcVK.png)*/ /* Chillout Mixer Christmas
+// background-image: url(http://i.imgur.com/nb4ibg4.png)*/ /* Chillout Mixer Chriatmas Lite
+// background-image: url(http://i.imgur.com/nptZvUk.png)*/ /* Chillout Mixer Theme
+// background-image: url(http://i.imgur.com/mL0fuwb.png)*/ /* Chillout Mixer Theme II
+// background-image: url(http://i.imgur.com/2Q89Rn2.png)*/ /* Christmas Classic
+// background-image: url(http://i.imgur.com/tOEACrk.png)*/ /* Digital Desert
+// background-image: url(http://i.imgur.com/zSMRtE6.png)*/ /* Digital Dungeon (Lite)
+// background-image: url(http://i.imgur.com/WTylHRy.png)*/ /* Digital Dungeon
+// background-image: url(http://i.imgur.com/6N7svVu.png)*/ /* End of Line Club
+// background-image: url(http://i.imgur.com/XZNVZmj.png)*/ /* Fairytale Land
+// background-image: url(http://i.imgur.com/9DVTnnW.png)*/ /* Mordor
+// background-image: url(http://i.imgur.com/8G44P7F.png)*/ /* (No Theme) Black
+// background-image: url(http://i.imgur.com/md2nlVM.png)*/ /* (No Theme) Mixer
+// background-image: url(http://i.imgur.com/zuRZwYR.png)*/ /* (No Theme) Plug
+// background-image: url(http://i.imgur.com/JZjGLPH.png)*/ /* Off The Grid V1
+// background-image: url(http://i.imgur.com/M4Z45oA.png)*/ /* Off The Grid V2
+// background-image: url(http://i.imgur.com/EFXFnql.png)*/ /* Orbital Lounge
+// background-image: url(http://i.imgur.com/P4GVhF4.png)*/ /* plug.dj Christmas Classic
+// background-image: url(http://i.imgur.com/M0CeHah.png)*/ /* plug.dj Chriatmas Ice
+// background-image: url(http://i.imgur.com/lK4GttQ.png)*/ /* Red Rocks Theme
+// background-image: url(http://i.imgur.com/N82wzhY.png)*/ /* Skygazer Theme
+// background-image: url(http://i.imgur.com/k9zVa92.png)*/ /* Skygazer Theme (Lite)
+// background-image: url(http://i.imgur.com/GZKgCpk.png)*/ /* TT.fm After Party Theme
+// background-image: url(http://i.imgur.com/u36VR4n.png)*/ /* TT.fm Red Theme
+// background-image: url(http://i.imgur.com/zlLjMmN.jpg)*/ /* BONUS: TASTYCAT
+// background-image: url(http://i.imgur.com/sfU5X2Q.jpg)*/ /* BONUS: HALLOWEEN '14
+// background-image: url(http://i.imgur.com/hjtFKu3.jpg)*/ /* BONUS: WINTER '13
+// background-image: url(http://i.imgur.com/S9qLJLn.jpg)*/ /* BONUS: WINTER '14
+// background finish
+// animation of coffee (below) (working progress!...)
+$(coffee_pic);
+function coffee_pic(){
+$('.span.emoji-2615').css('background','url("http://i.imgur.com/34ZjpvK.png")');}
+$(steam_firefox);
+function steam_firefox(){
+$('.span.emoji-2615').css('animation','steps(5,end), infinite');}
+$(steam_other);
+function steam_other(){
+$('.span.emoji-2615').css('-webkit-animation','steps(5,end), infinite');}
