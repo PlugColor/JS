813d289c451373e6b5cc575164076e0ced31a691
ureadmyname <ureadmyemailname@gmail.com>
2015-04-05 07:07:53 +0930

    Update PlugColor.js

--- PlugColor.js
+++ PlugColor.js
@@ -3,50 +3,49 @@ function background(){
 $('.room-background').css('background','url("http://i.imgur.com/EFXFnql.png")');}
 // 35 BACKGROUNDS:
 // (Just Change the URL (above) with a URL from(below))
-// 1: background-image: url(http://i.imgur.com/cwHjuod.png) Custom WINTER
-// 2: background-image: url(http://i.imgur.com/kmmPWiS.jpg) PLUG.DJ OLD DEFAULT
-// 3: background-image: url(http://i.imgur.com/1aw3xcd.png) Abandoned Ballroom
-// 4: background-image: url(http://i.imgur.com/HG8mqaM.png) Ancient Ruins
-// 5: background-image: url(http://i.imgur.com/8hfUntO.png) Architect Chamber
-// 6: background-image: url(http://i.imgur.com/XjiQctM.png) Bomb Shelter
-// 7: background-image: url(http://i.imgur.com/jNRQXKZ.png) Castleland (by Dionyus5)
-// 8: background-image: url(http://i.imgur.com/ILrUcVK.png) Chillout Mixer Christmas
-// 9: background-image: url(http://i.imgur.com/nb4ibg4.png) Chillout Mixer Chriatmas Lite
-// 10: background-image: url(http://i.imgur.com/nptZvUk.png) Chillout Mixer Theme
-// 11: background-image: url(http://i.imgur.com/mL0fuwb.png) Chillout Mixer Theme II
-// 12: background-image: url(http://i.imgur.com/2Q89Rn2.png) Christmas Classic
-// 13: background-image: url(http://i.imgur.com/tOEACrk.png) Digital Desert
-// 14: background-image: url(http://i.imgur.com/zSMRtE6.png) Digital Dungeon (Lite)
-// 15: background-image: url(http://i.imgur.com/WTylHRy.png) Digital Dungeon
-// 16: background-image: url(http://i.imgur.com/6N7svVu.png) End of Line Club
-// 17: background-image: url(http://i.imgur.com/XZNVZmj.png) Fairytale Land
-// 18: background-image: url(http://i.imgur.com/9DVTnnW.png) Mordor
-// 19: background-image: url(http://i.imgur.com/8G44P7F.png) (No Theme) Black
-// 20: background-image: url(http://i.imgur.com/md2nlVM.png) (No Theme) Mixer
-// 21: background-image: url(http://i.imgur.com/zuRZwYR.png) (No Theme) Plug
-// 22: background-image: url(http://i.imgur.com/JZjGLPH.png) Off The Grid V1
-// 23: background-image: url(http://i.imgur.com/M4Z45oA.png) Off The Grid V2
-// 24: background-image: url(http://i.imgur.com/EFXFnql.png) Orbital Lounge
-// 25: background-image: url(http://i.imgur.com/P4GVhF4.png) plug.dj Christmas Classic
-// 26: background-image: url(http://i.imgur.com/M0CeHah.png) plug.dj Chriatmas Ice
-// 27: background-image: url(http://i.imgur.com/lK4GttQ.png) Red Rocks Theme
-// 28: background-image: url(http://i.imgur.com/N82wzhY.png) Skygazer Theme
-// 29: background-image: url(http://i.imgur.com/k9zVa92.png) Skygazer Theme (Lite)
-// 30: background-image: url(http://i.imgur.com/GZKgCpk.png) TT.fm After Party Theme
-// 31: background-image: url(http://i.imgur.com/u36VR4n.png) TT.fm Red Theme
-// 32: background-image: url(http://i.imgur.com/zlLjMmN.jpg) BONUS: TASTYCAT
-// 33: background-image: url(http://i.imgur.com/sfU5X2Q.jpg) BONUS: HALLOWEEN '14
-// 34: background-image: url(http://i.imgur.com/hjtFKu3.jpg) BONUS: WINTER '13
-// 35: background-image: url(http://i.imgur.com/S9qLJLn.jpg) BONUS: WINTER '14
+// 1: background: url(http://i.imgur.com/cwHjuod.png) Custom WINTER
+// 2: background: url(http://i.imgur.com/kmmPWiS.jpg) PLUG.DJ OLD DEFAULT
+// 3: background: url(http://i.imgur.com/1aw3xcd.png) Abandoned Ballroom
+// 4: background: url(http://i.imgur.com/HG8mqaM.png) Ancient Ruins
+// 5: background: url(http://i.imgur.com/8hfUntO.png) Architect Chamber
+// 6: background: url(http://i.imgur.com/XjiQctM.png) Bomb Shelter
+// 7: background: url(http://i.imgur.com/jNRQXKZ.png) Castleland (by Dionyus5)
+// 8: background: url(http://i.imgur.com/ILrUcVK.png) Chillout Mixer Christmas
+// 9: background: url(http://i.imgur.com/nb4ibg4.png) Chillout Mixer Chriatmas Lite
+// 10: background: url(http://i.imgur.com/nptZvUk.png) Chillout Mixer Theme
+// 11: background: url(http://i.imgur.com/mL0fuwb.png) Chillout Mixer Theme II
+// 12: background: url(http://i.imgur.com/2Q89Rn2.png) Christmas Classic
+// 13: background: url(http://i.imgur.com/tOEACrk.png) Digital Desert
+// 14: background: url(http://i.imgur.com/zSMRtE6.png) Digital Dungeon (Lite)
+// 15: background: url(http://i.imgur.com/WTylHRy.png) Digital Dungeon
+// 16: background: url(http://i.imgur.com/6N7svVu.png) End of Line Club
+// 17: background: url(http://i.imgur.com/XZNVZmj.png) Fairytale Land
+// 18: background: url(http://i.imgur.com/9DVTnnW.png) Mordor
+// 19: background: url(http://i.imgur.com/8G44P7F.png) (No Theme) Black
+// 20: background: url(http://i.imgur.com/md2nlVM.png) (No Theme) Mixer
+// 21: background: url(http://i.imgur.com/zuRZwYR.png) (No Theme) Plug
+// 22: background: url(http://i.imgur.com/JZjGLPH.png) Off The Grid V1
+// 23: background: url(http://i.imgur.com/M4Z45oA.png) Off The Grid V2
+// 24: background: url(http://i.imgur.com/EFXFnql.png) Orbital Lounge
+// 25: background: url(http://i.imgur.com/P4GVhF4.png) plug.dj Christmas Classic
+// 26: background: url(http://i.imgur.com/M0CeHah.png) plug.dj Chriatmas Ice
+// 27: background: url(http://i.imgur.com/lK4GttQ.png) Red Rocks Theme
+// 28: background: url(http://i.imgur.com/N82wzhY.png) Skygazer Theme
+// 29: background: url(http://i.imgur.com/k9zVa92.png) Skygazer Theme (Lite)
+// 30: background: url(http://i.imgur.com/GZKgCpk.png) TT.fm After Party Theme
+// 31: background: url(http://i.imgur.com/u36VR4n.png) TT.fm Red Theme
+// 32: background: url(http://i.imgur.com/zlLjMmN.jpg) BONUS: TASTYCAT
+// 33: background: url(http://i.imgur.com/sfU5X2Q.jpg) BONUS: HALLOWEEN '14
+// 34: background: url(http://i.imgur.com/hjtFKu3.jpg) BONUS: WINTER '13
+// 35: background: url(http://i.imgur.com/S9qLJLn.jpg) BONUS: WINTER '14
 // animation of coffee (below) (working progress!...)
 $(coffee_pic);
 function coffee_pic(){
 $('.span.emoji-2615').css('background','url("http://i.imgur.com/34ZjpvK.png")');}
-$('.span.emoji-2615')(function(){
-$(this).animate({'background-position':'0px, 0px'}, 1000);
-}, function() {
-$(this).animate({'background-position':'-80px, 0px'}, 1000);
-});
+$('.span.emoji-2615').animate({
+  'background-position-x': '0%',
+  'background-position-y': '400%'
+}, 1000, 'linear');
 $('#dj-button span').hover(function(){
 $('#dj-button span').css('color','purple');
 }, function(){
