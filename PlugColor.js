8a83127e3efdd667e0a287eff29e9c6d5251e49b
ureadmyname <ureadmyemailname@gmail.com>
2015-04-07 05:42:46 +0930

    Update PlugColor.js

--- PlugColor.js
+++ PlugColor.js
@@ -41,11 +41,7 @@ $('#dj-button span').css('color','purple');},
 function(){
 $('#dj-button span').css('color','white');});
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // /
-div.inventory.button:hover{
-background: linear-gradient(bottom, #FCFCFC 100%);
-background: -o-linear-gradient(bottom, #FCFCFC 100%);
-background: -moz-linear-gradient(bottom, #FCFCFC 100%);
-background: -webkit-linear-gradient(bottom, #FCFCFC 100%);
-background: -ms-linear-gradient(bottom, #FCFCFC 100%);
+('#footer-user .buttons .button.settings:hover').css({
+background: "-moz-linear-gradient(left, #FF0000 0%, #00FFFF 100%)"});
 $('span.emoji-2615').css('background', 'url("http://i.imgur.com/34ZjpvK.png")');
 $('.badge-box.no-badge i').css('background', 'url("http://i.imgur.com/geekZoB.png")');
