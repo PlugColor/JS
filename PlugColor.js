6be49c63e063798a4173b1d199422f01afac8bca
Jason <ureadmyemailname@gmail.com>
2015-05-02 11:30:05 +0930

    Update PlugColor.js

--- PlugColor.js
+++ PlugColor.js
@@ -2,4 +2,4 @@ var linkElem = document.createElement('link');
 document.getElementsByTagName('head')[0].appendChild(linkElem);
 linkElem.rel = 'stylesheet';
 linkElem.type = 'text/css';
-linkElem.href = 'https://raw.githubusercontent.com/PlugColor/CSS/master/PlugColor.css';
+linkElem.href = 'https://rawgit.com/PlugColor/CSS/master/PlugColor.css';
